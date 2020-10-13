495159f8dca576e29703a4f7df0daf91fe90436d
denis.ivashkov <ivashkovdv@gmail.com>
2020-10-13 21:52:39 +0300

    update N

--- nnkek/plotters.py
+++ nnkek/plotters.py
@@ -9,10 +9,13 @@ from nnkek import imagers
 
 
 def im_grid(images, nrows=2, ncols=5, figsize=(6, 8), imgsize=(128, 128)):
+    # это негибко. Лучше, чтобы кол-во картинок было неожиданным
 
     fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
 
-    for i, axi in enumerate(ax.flat):
+    ax = ax.flat if nrows > 1 else ax
+
+    for i, axi in enumerate(ax):
         img = images[i]
         imagers.imshow(img, axi, imgsize)
 
